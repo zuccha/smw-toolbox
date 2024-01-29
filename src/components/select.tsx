3113154c11c3d857a0297ea7d721b383945bf51a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-01-29 12:11:52 +0100

    feat: uniform inputs styles

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,6 +1,7 @@
-import { useCallback } from "preact/hooks";
-import "./select.css";
 import { ChevronDown } from "lucide-preact";
+import { useCallback } from "preact/hooks";
+import { classNames } from "../utils";
+import "./select.css";
 
 export type SelectOption<T extends number> = { label: string; value: T };
 
@@ -26,13 +27,37 @@ export default function Select<T extends number>({
 
   return (
     <div class="select">
-      <select onChange={handleChange} placeholder={placeholder} value={value}>
+      <select
+        class="hidden"
+        onChange={handleChange}
+        placeholder={placeholder}
+        value={value}
+      >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
           </option>
         ))}
       </select>
+      <div class="select-labels">
+        {options.map((option) => {
+          const className = classNames([
+            ["select-label", true],
+            ["hidden", option.value !== value],
+          ]);
+          return (
+            <div class={className} key={option.value}>
+              {option.label}
+            </div>
+          );
+        })}
+      </div>
+
+      {options.some((option) => option.value === value) ? (
+        <div class="select-value">{"\u200B"}</div>
+      ) : (
+        <div class="select-placeholder">{placeholder}</div>
+      )}
 
       <div class="select-icon">
         <ChevronDown size="1em" />
