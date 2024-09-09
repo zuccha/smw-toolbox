d66f4a7737977e544e455290d06a9255862f5a29
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-09-09 18:30:07 +0200

    feat: add sizes to setting component

--- src/components/setting.tsx
+++ src/components/setting.tsx
@@ -5,15 +5,21 @@ type Setting = {
   children: ReactNode;
   hotkey?: string;
   label: string;
+  size?: "md" | "lg";
 };
 
-export default function Setting({ children, hotkey, label }: Setting) {
+export default function Setting({
+  children,
+  hotkey,
+  label,
+  size = "lg",
+}: Setting) {
   return (
     <div class="Setting">
       <span class="Setting_Label">
         {hotkey ? `${label} (${hotkey})` : label}
       </span>
-      <div class="Setting_Input">{children}</div>
+      <div class={`Setting_Input ${size}`}>{children}</div>
     </div>
   );
 }
