b84ad9d392445e09210b9cf91699bdea0fcb70a0
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-09-21 15:03:27 +0200

    feat: add links to bug report and feature request

--- src/modules/app-info/app-info-section-about.tsx
+++ src/modules/app-info/app-info-section-about.tsx
@@ -1,3 +1,4 @@
+import { ExternalLinkIcon } from "lucide-preact";
 import SectionCollapsible from "../../components/section-collapsible";
 import { useAppInfoTabAboutIsVisible } from "./store";
 
@@ -18,7 +19,27 @@ export default function AppInfoSectionAbout() {
       <div>©2024 zuccha</div>
       <div>
         <a href="https://github.com/zuccha/smw-toolbox" target="_blank">
-          Source code
+          Source code <ExternalLinkIcon size="0.8em" />
+        </a>
+      </div>
+      <br />
+      <div>
+        <b>Contributing</b>
+      </div>
+      <div>
+        <a
+          href="https://github.com/zuccha/smw-toolbox/issues/new?assignees=&labels=bug&projects=&template=bug_report.md&title="
+          target="_blank"
+        >
+          Report a bug <ExternalLinkIcon size="0.8em" />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://github.com/zuccha/smw-toolbox/issues/new?assignees=&labels=enhancement&projects=&template=feature_request.md&title="
+          target="_blank"
+        >
+          Suggest a feature <ExternalLinkIcon size="0.8em" />
         </a>
       </div>
     </SectionCollapsible>
