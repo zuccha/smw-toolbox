a0f3c70c46db0bb1b49e32f92086dd26a02339d6
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-09-21 10:06:44 +0200

    build: split code into chunks on build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [preact()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          codemirror: ["@codemirror/language", "@codemirror/state"],
+          uiw: ["@uiw/codemirror-themes", "@uiw/react-codemirror"],
+        },
+      },
+    },
+  },
 });
