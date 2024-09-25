78bfc756a80af830bed223614fa76170269fc48b
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-09-25 14:24:27 +0200

    fix: correctly compute branches length

--- src/extra/asm65816/emulator/instructions/b__.ts
+++ src/extra/asm65816/emulator/instructions/b__.ts
@@ -6,71 +6,74 @@ abstract class B___Near extends Instruction {
 
   public static base_cycles = 2;
 
+  protected _branch_taken = false;
+
   public get cycles(): number {
-    return this._branch_taken()
-      ? B___Near.base_cycles + 1 + this.p.flag_e // TODO: Add e only if branch crossed page boundary.
+    return this._branch_taken
+      ? B___Near.base_cycles + 1 + this._snapshot_before.flag_e // TODO: Add e only if branch crossed page boundary.
       : B___Near.base_cycles;
   }
 
-  protected abstract _branch_taken(): boolean;
+  protected abstract check_branch(): boolean;
 
   public execute_effect(): void {
-    if (this._branch_taken()) this.p.pc = this.addr;
+    this._branch_taken = this.check_branch();
+    if (this._branch_taken) this.p.pc = this.addr;
   }
 }
 
 export class BCC extends B___Near {
   public static mnemonic = "BCC";
   public static opcode = 0x90;
-  protected _branch_taken = () => this.p.flag_c === 0;
+  protected check_branch = () => this.p.flag_c === 0;
 }
 
 export class BCS extends B___Near {
   public static mnemonic = "BCS";
   public static opcode = 0xb0;
-  protected _branch_taken = () => this.p.flag_c === 1;
+  protected check_branch = () => this.p.flag_c === 1;
 }
 
 export class BNE extends B___Near {
   public static mnemonic = "BNE";
   public static opcode = 0xd0;
-  protected _branch_taken = () => this.p.flag_z === 0;
+  protected check_branch = () => this.p.flag_z === 0;
 }
 
 export class BEQ extends B___Near {
   public static mnemonic = "BEQ";
   public static opcode = 0xf0;
-  protected _branch_taken = () => this.p.flag_z === 1;
+  protected check_branch = () => this.p.flag_z === 1;
 }
 
 export class BPL extends B___Near {
   public static mnemonic = "BPL";
   public static opcode = 0x10;
-  protected _branch_taken = () => this.p.flag_n === 0;
+  protected check_branch = () => this.p.flag_n === 0;
 }
 
 export class BMI extends B___Near {
   public static mnemonic = "BMI";
   public static opcode = 0x30;
-  protected _branch_taken = () => this.p.flag_n === 1;
+  protected check_branch = () => this.p.flag_n === 1;
 }
 
 export class BVC extends B___Near {
   public static mnemonic = "BVC";
   public static opcode = 0x50;
-  protected _branch_taken = () => this.p.flag_v === 0;
+  protected check_branch = () => this.p.flag_v === 0;
 }
 
 export class BVS extends B___Near {
   public static mnemonic = "BVS";
   public static opcode = 0x70;
-  protected _branch_taken = () => this.p.flag_v === 1;
+  protected check_branch = () => this.p.flag_v === 1;
 }
 
 export class BRA extends B___Near {
   public static mnemonic = "BRA";
   public static opcode = 0x80;
-  protected _branch_taken = () => true;
+  protected check_branch = () => true;
 }
 
 export class BRL extends Instruction {
