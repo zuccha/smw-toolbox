1e492c47b6e709786a7e94ee88c5bfcee9f38ee7
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2024-10-01 14:59:18 +0200

    test: write stack pull tests

--- src/extra/asm65816/tests/ph_.test.ts
+++ src/extra/asm65816/tests/ph_.test.ts
@@ -1,17 +1,17 @@
 import { describe, test } from "vitest";
 import { run } from "./_run";
 
-describe("'addr'", () => {
+describe("'implied'", () => {
   test.each`
     opcode   | initialProcessor                        | expectedProcessor | expectedMemory
-    ${"PHA"} | ${{ a: 0x1234, sp: 0x1fc0 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
-    ${"PHA"} | ${{ a: 0x1234, sp: 0x1fc0, flag_m: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
-    ${"PHX"} | ${{ x: 0x1234, sp: 0x1fc0 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
-    ${"PHX"} | ${{ x: 0x1234, sp: 0x1fc0, flag_x: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
-    ${"PHY"} | ${{ y: 0x0034, sp: 0x1fc0 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
-    ${"PHY"} | ${{ y: 0x1234, sp: 0x1fc0, flag_x: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
-    ${"PHB"} | ${{ db: 0xab, sp: 0x1fc0 }}             | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0xab]])}
-    ${"PHD"} | ${{ dp: 0x0310, sp: 0x1fc0 }}           | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x10], [0x7e1fc0, 0x03]])}
+    ${"PHA"} | ${{ sp: 0x1fc0, a: 0x1234 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
+    ${"PHA"} | ${{ sp: 0x1fc0, a: 0x1234, flag_m: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
+    ${"PHX"} | ${{ sp: 0x1fc0, x: 0x1234 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
+    ${"PHX"} | ${{ sp: 0x1fc0, x: 0x1234, flag_x: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
+    ${"PHY"} | ${{ sp: 0x1fc0, y: 0x1234 }}            | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x34]])}
+    ${"PHY"} | ${{ sp: 0x1fc0, y: 0x1234, flag_x: 0 }} | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x34], [0x7e1fc0, 0x12]])}
+    ${"PHB"} | ${{ sp: 0x1fc0, db: 0xab }}             | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0xab]])}
+    ${"PHD"} | ${{ sp: 0x1fc0, dp: 0x0310 }}           | ${{ sp: 0x1fbe }} | ${new Map([[0x7e1fbf, 0x10], [0x7e1fc0, 0x03]])}
     ${"PHK"} | ${{ sp: 0x1fc0 }}                       | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0x80]])}
     ${"PHP"} | ${{ sp: 0x1fc0 }}                       | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0b00110000]])}
     ${"PHP"} | ${{ sp: 0x1fc0, flag_n: 1, flag_x: 0 }} | ${{ sp: 0x1fbf }} | ${new Map([[0x7e1fc0, 0b10100000]])}
